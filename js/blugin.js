aa7ab58d60ff2c3867ba9c61ae354fad2fe8a4ae
Ahmed Safwat <63418052+Ahmed-safwat-2020@users.noreply.github.com>
2020-06-28 00:35:08 +0200

    .

--- js/blugin.js
+++ js/blugin.js
@@ -2,12 +2,4 @@ $(document).ready(function(){
     $(".width .dropdown-menu").css("width",$(window).width() - 45)
     $(".main,.main .content").css("height",$(window).height());
     
-    $(".popular .content").on({
-        mouseenter:function(){
-            $(this).css("marginBottom","30px")}
-            ,
-        mouseleave:function(){
-            $(this).css("marginBottom","0px")
-        }
-    })
 })
