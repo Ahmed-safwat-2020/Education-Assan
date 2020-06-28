86f964a1cf10c1cc101d613a168b7cb53c904085
Ahmed Safwat <63418052+Ahmed-safwat-2020@users.noreply.github.com>
2020-06-28 19:07:40 +0200

    Update blugin.js

--- js/blugin.js
+++ js/blugin.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".width .dropdown-menu").css("width",$(window).width() - 30)
+    $(".width .dropdown-menu").css("width",$(window).width() - 45)
     $(".main,.main .content").css("height",$(window).height());
     
     $(window).on({
